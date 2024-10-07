75bae64a6c60b49112a3a70e1f25411f9b5e2566
annabel-anh <annabel.anh@gmail.com>
2024-10-07 17:38:19 -0600

    done happy path plant controller

--- src/controllers/plant.controllers.ts
+++ src/controllers/plant.controllers.ts
@@ -3,10 +3,6 @@ import httpResponses from "../utils/httpResponses.utils"
 import logger from "../utils/logger"
 
 export const getPlants = async (req, res) => {
-    if (!req.user) {
-        return httpResponses.authorizationRequired(res)
-    }
-
     try {
         const plants = await prisma.plant.findMany({
             where: {
@@ -24,16 +20,11 @@ export const getPlants = async (req, res) => {
 }
 
 export const createPlant = async (req, res) => {
-    if (!req.user) {
-        return httpResponses.authorizationRequired(res)
-    }
-
     if (!req.body || Object.keys(req.body).length === 0) {
         return httpResponses.badRequest(res, "Content can't be empty!")
     }
 
     const user_id = req.user.id
-    console.log(req.body)
     const { name, ...rest } = req.body
 
     if (!name) {
@@ -50,5 +41,57 @@ export const createPlant = async (req, res) => {
         return httpResponses.created(res, newPlant)
     } catch (error) {
         logger.error("Error creating plant", { error: error.message })
+        return httpResponses.interalServerError(res)
+    }
+}
+
+export const getOnePlant = async (req, res) => {
+    try {
+        const plantId = Number(req.params.id)
+        const plant = await prisma.plant.findUniqueOrThrow({
+            where: {
+                id: plantId,
+            },
+        })
+        console.log(plant)
+        return httpResponses.sendSuccess(res, 200, plant)
+    } catch (error) {
+        logger.error("Error finding plant", { error: error.message })
+        return httpResponses.notFound(res, "Plant not found.")
+    }
+}
+
+export const editPlant = async (req, res) => {
+    try {
+        if (!req.body || Object.keys(req.body).length === 0) {
+            return httpResponses.badRequest(res, "Content can't be empty!")
+        }
+
+        if (!req.body.name) {
+            return httpResponses.badRequest(res, "Plant name can't be empty")
+        }
+
+        const plantId = Number(req.params.id)
+        const plant = await prisma.plant.update({
+            where: { id: plantId },
+            data: { ...req.body },
+        })
+        return httpResponses.updated(res, plant)
+    } catch (error) {
+        logger.error("Error finding plant", { error: error.message })
+        return httpResponses.notFound(res, "Plant not found.")
+    }
+}
+
+export const deletePlant = async (req, res) => {
+    try {
+        const plantId = Number(req.params.id)
+        const plant = await prisma.plant.delete({
+            where: { id: plantId },
+        })
+        return httpResponses.deleted(res)
+    } catch (error) {
+        logger.error("Error finding plant", { error: error.message })
+        return httpResponses.notFound(res, "Plant not found.")
     }
 }
--- src/routes/plant.routes.ts
+++ src/routes/plant.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express"
-import { createPlant, getPlants } from "../controllers/plant.controllers"
+import {
+    createPlant,
+    getPlants,
+    getOnePlant,
+    editPlant,
+    deletePlant,
+} from "../controllers/plant.controllers"
 
 const router = Router()
 
 router.route("/").get(getPlants).post(createPlant)
 
-router.route("/:id").get().put().delete()
+router.route("/:id").get(getOnePlant).put(editPlant).delete(deletePlant)
 
 export default router
